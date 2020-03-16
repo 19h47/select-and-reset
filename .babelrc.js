94957de754bce551dc2fb37bb0ffd5f3e1b101f8
Levron Jérémy <jeremylevron@19h47.fr>
2020-03-16 08:27:05 +0100

    :arrow_up: UP

--- .babelrc.js
+++ .babelrc.js
@@ -16,7 +16,9 @@ const presets = [
 ];
 
 const plugins = [
-	"@babel/plugin-transform-runtime",
+	'@babel/plugin-syntax-dynamic-import',
+	'@babel/plugin-transform-runtime',
+	'@babel/plugin-transform-parameters',
 ];
 
 module.exports = { presets, plugins };
